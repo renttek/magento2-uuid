a952d6a39669eedf8100f05ba5a1319367b357ad
Julian Nuß <jn@integer-net.de>
2022-10-01 14:52:59 +0200

    Convert url params to Uuid/Ulid

---
+++ src/Plugin/UidParameterConverter.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Renttek\Uuid\Plugin;
+
+use Magento\Framework\Webapi\ServiceInputProcessor;
+use Renttek\Uuid\Api\RamseyUuid;
+use Renttek\Uuid\Api\SymfonyUid;
+
+class UidParameterConverter
+{
+    public function aroundConvertValue(
+        ServiceInputProcessor $processor,
+        callable $proceed,
+        mixed $data,
+        string $type
+    ) {
+        if (!is_string($data)) {
+            return $proceed($data, $type);
+        }
+
+        return match($type) {
+            RamseyUuid\Uuid::class => new RamseyUuid\Uuid($data),
+            SymfonyUid\Uuid::class => new SymfonyUid\Uuid($data),
+            SymfonyUid\Ulid::class => new SymfonyUid\Ulid($data),
+            default => $proceed($data, $type),
+        };
+    }
+}
